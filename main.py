3cdcb2c80ae0c1b86ed6a0e36797c263e949b654
Mrs. Ramirez Ramirez <>
2021-04-28 19:13:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+#This program will draw an emoji face with eyes and a mouth
+
+import turtle
+
+#create the turtle object, set the speed and hide the turtle
+t = turtle.Turtle()
+t.speed(0)
+t.ht()
+
+#set the position (x,y coordinate) of the turtle to begin drawing
+#set the turtle facing right
+t.setpos(0,-200)
+t.setheading(0)
+t.pensize(5)      #set the pensize-this can be changed
+t.pendown()       #set the pen down to begin drawing
+
+
+#draw the face (big circle)
+
+
+
+#draw left eye
+
+
+
+
+#draw right eye
+
+
+
+
+#draw mouth
+
